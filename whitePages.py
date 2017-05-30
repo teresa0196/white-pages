f53e73c2cca67ff71b384e05d25157cef5edd039
Teresa Jeon <teresajeon@seoulchristian.org>
2017-05-30 22:04:47 +0900

    Add comments in the code

--- whitePages.py
+++ whitePages.py
@@ -1,46 +1,39 @@
-print ("Welcome! ") # Print the message
+print ("WELCOME!")# Print the message
 
 
-enter = raw_input("Press ENTER to continue ")
+enter = raw_input("Press ENTER to continue... ") # set the variable named enter to get input from the user
 
-if enter == "" :
-    filename = raw_input("Set the filename. >>")
+if enter == "" : # set how to response when the enter variable gets nothing for the input
+    filename = raw_input("Set the file name. >> ") # create a filename variable that gets the input from the user
 
-    with open(filename, 'w') as p:
-        print "Now, you're going to type 3 people's name, birthday, and phone number."
+    with open(filename, 'w') as p: # open the file that user had created above in writing mode
+        print ("\nOkay, so your file name is <" + filename + ">.\n\nNow, you're going to type 3 people's name, birthday, and phone number.") # print the message
 
-        print ("<First Person's Info>")
-        line1 = raw_input("Name: ")
-        p.write(line1)
-        p.write("\n")
-        line2 = raw_input("Birthday: ")
-        p.write(line2)
-        f.write("\n")
-        line3 = raw_input("Phone Number:")
-        p.write(line3)
-        p.write("\n\n")
+        print ("\n<First Person's Info>") # print the message
+        name = raw_input("Name: ") # create name variable as the input from the user
+        p.write(name + "\n") # write name in the text file
+        bday = raw_input("Birthday: ") # craete bday variable as the input from the user
+        p.write(bday + "\n") # write bday in the text file
+        pNumber = raw_input("Phone Number:") # create pNumber variable as the input from the user
+        p.write(pNumber + "\n\n") # write pNumber in the text file
 
-        print ("\n<Second Person's Info>")
-        line1 = raw_input("Name: ")
-        p.write(line1)
-        p.write("\n")
-        line2 = raw_input("Birthday: ")
-        p.write(line2)
-        p.write("\n")
-        line3 = raw_input("Phone Number:")
-        p.write(line3)
-        p.write("\n\n")
 
-        print ("\n<Third Person's Info>")
-        line1 = raw_input("Name: ")
-        p.write(line1)
-        p.write("\n")
-        line2 = raw_input("Birthday: ")
-        p.write(line2)
-        p.write("\n")
-        line3 = raw_input("Phone Number:")
-        p.write(line3)
-        p.write("\n\n")
+        print ("\n<Second Person's Info>") # print the message
+        name = raw_input("Name: ") # create name variable as the input from the user
+        p.write(name + "\n") # write name in the text file
+        bday = raw_input("Birthday: ") # create bday variable as the input from the user
+        p.write(bday + "\n")  # write bday in the text file
+        pNumber = raw_input("Phone Number:") # create pNumber variable as the input from the user
+        p.write(pNumber + "\n\n") # write pNumber in the text file
 
-        print "\nThe file is saved. Bye!"
-        p.close()
+        print ("\n<Third Person's Info>") # print the message
+        name = raw_input("Name: ") # create name variable as the input from the user
+        p.write(name + "\n")  # write name in the text file
+        bday = raw_input("Birthday: ") # create bday variable as the input from the user
+        p.write(bday + "\n") # write bday in the text file
+        pNumber = raw_input("Phone Number:") # create pNumber variable as the input from the user
+        p.write(pNumber + "\n\n") # write pNumber in the text file
+
+
+        print ("\nYour file <" + filename + "> is saved. Thank you!") # print the message
+        p.close() # close the file
